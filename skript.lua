84ad8032120623a895919e64ab591ad7b2aabc0e
bubatz <71530056+cynhook@users.noreply.github.com>
2022-05-26 20:55:06 -0400

    Create skript.lua

---
+++ skript.lua
@@ -0,0 +1,10 @@
+local Notification = loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/IrisBetterNotifications.lua"))()
+
+Notification.Notify("Neverlocking", "private", "9734900288", {
+Duration = 2,       
+Main = {
+    Rounding = true,
+}
+});
+local main1 = loadstring(game:HttpGet("https://raw.githubusercontent.com/neverlackinga/better/main/gui/guiC.lua"))()
+local main2 = main1.CreateLib("Neverlocking.wtf", "Midnight")
